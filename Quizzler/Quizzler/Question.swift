ea11adcfaad1dd06f6f4cc431ba885f6d419b1b9
Hussain <hussainarthuna@gmail.com>
2019-01-17 21:25:26 +0300

    In iOS

---
+++ Quizzler/Quizzler/Question.swift
@@ -0,0 +1,25 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Hussain Arthuna on 1/17/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Question{
+    
+    let questionText : String
+    let answer : Bool
+    
+    init(text : String, correctAnswer : Bool) {
+        
+        questionText = text
+        answer = correctAnswer
+        
+    }
+    
+    
+    
+}
---
+++ Quizzler/Quizzler/Quizzler-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Quizzler-Bridging-Header.h
+//  Quizzler
+//
+//  Created by Hussain Arthuna on 1/17/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+#import "ProgressHUD.h"
+#ifndef Quizzler_Bridging_Header_h
+#define Quizzler_Bridging_Header_h
+
+
+#endif /* Quizzler_Bridging_Header_h */
