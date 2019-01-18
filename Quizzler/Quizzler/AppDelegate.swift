0bcb16c53d82782ec728e16501ba136fc2e0fb85
Hussain <hussainarthuna@gmail.com>
2019-01-18 08:33:50 +0300

    Bug fixes

--- Quizzler/Quizzler/AppDelegate.swift
+++ Quizzler/Quizzler/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  Quizzler
 //
-//  Created by Angela Yu on 25/08/2015.
-//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//  Created by Hussain Arthuna on 1/16/19.
+//  Copyright © 2019 Hussain Arthuna. All rights reserved.
 //
 
 import UIKit
